439138ca4036c0fd6a7141c6234f3b0b3ee51219
OscarHansson72 <oscar_oh_94@hotmail.com>
2025-05-13 15:10:05 +0200

    added a listgroup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
-import Message from './Message'
-function App(){
-  return <div><Message/></div>
+import ListGroup from "./components/ListGroup";
+function App() {
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
